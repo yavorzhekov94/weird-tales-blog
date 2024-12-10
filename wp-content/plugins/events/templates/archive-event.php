a87e0e417d3a98d96063a5c7e52132926e7914a7
Yavor Zhekov <yavor_zhekov@abv.bg>
2024-12-10 02:09:26 +0200

    New improvements

--- wp-content/plugins/events/templates/archive-event.php
+++ wp-content/plugins/events/templates/archive-event.php
@@ -1,16 +1,13 @@
 <?php 
-    get_header(); ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"> All events</h1>
-            <div class="page-banner__intro">
-            <p> Your event is my event. Keep this on!</p>
-            </div>
-        </div>
-    </div>
-    <div class="container container--narrow page-section">
-        <?php
+    get_header();
+    page_banner(
+      array(
+          'title' => 'All Events',
+          'subtitle' => "LOve my love events"
+        )
+      ); ?>
+    
+      <?php
        
        while(have_posts()) {
 
--- wp-content/themes/weird-tales/functions.php
+++ wp-content/themes/weird-tales/functions.php
@@ -22,6 +22,7 @@ function weird_tales_features () {
     add_theme_support('post-thumbnails');
     add_image_size('albumLandScape', 400, 260, true);
     add_image_size('albumPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 add_action('after_setup_theme', 'weird_tales_features');
 
@@ -45,5 +46,32 @@ function adjust_queries($query) {
     }
     
 }
-add_action('pre_get_posts', 'adjust_queries')
+add_action('pre_get_posts', 'adjust_queries');
+
+function page_banner($args = NULL) {
+    if (!isset($args['title'])) {
+        $args['title'] = get_the_title();
+    }
+    if (!isset($args['subtitle'])) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+    if (!isset($args['photo'])) {
+        if (get_field('page_banner_image') && !is_archive() && !is_home()) {
+            $args['photo'] = get_field('page_banner_image')['sizes']['pageBanner'];
+
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+    ?>
+     <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'];?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"> <?php echo $args['title']; ?></h1>
+            <div class="page-banner__intro">
+            <p><?php echo $args['subtitle'];?></p>
+            </div>
+        </div>
+    </div>
+<?php }
 ?>
